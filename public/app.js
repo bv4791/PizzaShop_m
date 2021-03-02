f8b2923edae040324ec3f049324f818c474f6a44
bv4791 <bv4791@gmail.com>
2021-03-02 09:43:34 +0200

    Add cart_get_number_of_items fanction

--- public/app.js
+++ public/app.js
@@ -22,5 +22,24 @@ function add_to_cart(id)
 
 	window.localStorage.setItem(key, x);
 
-	alert(x);
+	alert('Items in your cart:' + cart_get_number_of_items());
+}
+
+
+function cart_get_number_of_items()
+{
+  var cnt = 0;
+
+  for (var i = 0; i < window.localStorage.length; i++)
+  {
+    var key = window.localStorage.key(i); // получаем ключ
+    var value = window.localStorage.getItem(key); // получаем значение
+
+    if(key.indexOf('product_') == 0)
+    {
+      cnt = cnt + value * 1;
+    }
+  }
+
+  return cnt;
 }
