1ffc5bea99e5f27be5fad5042115d9c1e7d80076
bv4791 <bv4791@gmail.com>
2021-03-02 13:46:19 +0200

    Update orders button

--- public/app.js
+++ public/app.js
@@ -23,6 +23,7 @@ function add_to_cart(id)
 	window.localStorage.setItem(key, x);
 
 	update_orders_input();
+  update_orders_button();
 }
 
 
@@ -30,6 +31,12 @@ function update_orders_input()
 {
   var orders = cart_get_orders();
   $('#orders_input').val(orders);
+}
+
+function update_orders_button()
+{
+  var text = 'Cart ('+ cart_get_number_of_items() +')';
+   $('#orders_button').val(text);
 }
 
 
